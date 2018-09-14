787aa535095ba0a473901cae9b2939406f533515
NetkoNefarious <jmagazin5@gmail.com>
2018-09-14 12:08:06 +0200

    Added some AJAX functionality and a question to each quiz

--- js/script.js
+++ js/script.js
@@ -1,10 +1,33 @@
-function LoadGeography () {
+function LoadForm (str) {
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             document.getElementsByTagName('main')[0].innerHTML = this.responseText;
         }
     }
-    xhr.open('GET', 'ajax/geo.html', true);
+    xhr.open('GET', 'ajax/form.html', true);
+    xhr.send();
+
+    $(document).on('click', 'button', function(event) { LoadQuiz(str); })
+}
+
+function LoadQuiz (str) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementsByTagName('main')[0].innerHTML = this.responseText;
+        }
+    }
+
+    switch (str) {
+        case 'geo':
+            var file = 'ajax/geo.html';
+        case 'psy':
+            var file = 'ajax/psy.html';
+        case 'game':
+            var file = 'ajax/game.html';
+    }
+
+    xhr.open('GET', file, true);
     xhr.send();
 }
